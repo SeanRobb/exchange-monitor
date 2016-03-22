a303a2338d6e548023813c43c91cb69d8f9a2c45
SeanRobb <sean.robb@centurylink.com>
2016-03-22 09:02:33 -0500

    reverted to old message

--- src/main/java/work/hoodie/crypto/exchange/monitor/service/notification/message/builder/EmailMessageBuilderService.java
+++ src/main/java/work/hoodie/crypto/exchange/monitor/service/notification/message/builder/EmailMessageBuilderService.java
@@ -1,12 +1,16 @@
 package work.hoodie.crypto.exchange.monitor.service.notification.message.builder;
 
 import com.xeiam.xchange.ExchangeSpecification;
+import com.xeiam.xchange.currency.CurrencyPair;
+import com.xeiam.xchange.dto.Order;
 import com.xeiam.xchange.dto.trade.UserTrade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import work.hoodie.crypto.exchange.monitor.domain.EmailMessage;
 import work.hoodie.crypto.exchange.monitor.service.TradeConverter;
+
+import java.math.BigDecimal;
 
 @Component
 public class EmailMessageBuilderService {
@@ -20,21 +24,48 @@ public class EmailMessageBuilderService {
     @Value("${email.address:}")
     private String emailAddress;
 
+    private final String purchased = "purchased";
+    private final String sold = "sold";
+
     public EmailMessage build(UserTrade userTrade) {
         EmailMessage emailMessage = new EmailMessage();
         emailMessage.setSubject(exchangeSpecification.getExchangeName() + " Monitor");
 
-        emailMessage.setContent( "Received " +
-                tradeConverter.getCoinReceived(userTrade).toPlainString() + " " +
-                tradeConverter.getCoinReceivedName(userTrade) + " for " +
-                tradeConverter.getCoinSent(userTrade).toPlainString() + " " +
-                tradeConverter.getCoinSentName(userTrade) + " with "
-                + userTrade.getFeeAmount().toPlainString() + " " + userTrade.getFeeCurrency() + " in fees.");
+        Order.OrderType type = userTrade.getType();
+        BigDecimal amount = userTrade.getTradableAmount();
+        CurrencyPair currencyPair = userTrade.getCurrencyPair();
+        BigDecimal price = userTrade.getPrice();
+
+
+        BigDecimal feeAmount = userTrade.getFeeAmount();
+        String feeCurrency = userTrade.getFeeCurrency();
+        BigDecimal total = amount.multiply(price);
+        BigDecimal feesInCoins = price.multiply(feeAmount);
+        BigDecimal actualAmount = total.subtract(feesInCoins);
+
+        String message = actualAmount + " " + currencyPair.baseSymbol + " " + typeConvert(type) +
+                " for " + price + " " + currencyPair.counterSymbol +
+                " \n Fees Payed: " + feeAmount + " " + feeCurrency +
+                " \n Total Payed: " + total + " " + feeCurrency;
+        emailMessage.setContent(message);
 
         emailMessage.setFromEmailAddress(fromEmailAddress);
 
         emailMessage.setToEmailAddress(emailAddress);
 
         return emailMessage;
+    }
+
+
+    private String typeConvert(Order.OrderType type) {
+        String tradeType;
+        if (type == Order.OrderType.BID) {
+            tradeType = purchased;
+        } else if (type == Order.OrderType.ASK) {
+            tradeType = sold;
+        } else {
+            tradeType = "";
+        }
+        return tradeType;
     }
 }
--- src/main/java/work/hoodie/crypto/exchange/monitor/service/notification/message/builder/SlackMessageBuilderService.java
+++ src/main/java/work/hoodie/crypto/exchange/monitor/service/notification/message/builder/SlackMessageBuilderService.java
@@ -1,12 +1,15 @@
 package work.hoodie.crypto.exchange.monitor.service.notification.message.builder;
 
 import com.xeiam.xchange.ExchangeSpecification;
+import com.xeiam.xchange.currency.CurrencyPair;
 import com.xeiam.xchange.dto.Order.OrderType;
 import com.xeiam.xchange.dto.trade.UserTrade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import work.hoodie.crypto.exchange.monitor.domain.SlackMessage;
 import work.hoodie.crypto.exchange.monitor.service.TradeConverter;
+
+import java.math.BigDecimal;
 
 @Component
 public class SlackMessageBuilderService {
@@ -25,14 +28,37 @@ public class SlackMessageBuilderService {
     public SlackMessage build(UserTrade userTrade) {
         username = exchangeSpecification.getExchangeName() + " Monitor";
 
-        String message = "Received " +
-                tradeConverter.getCoinReceived(userTrade).toPlainString() + " " +
-                tradeConverter.getCoinReceivedName(userTrade) + " for " +
-                tradeConverter.getCoinSent(userTrade).toPlainString() + " " +
-                tradeConverter.getCoinSentName(userTrade) + " with "
-                + userTrade.getFeeAmount().toPlainString() + " " + userTrade.getFeeCurrency() + " in fees.";
+        OrderType type = userTrade.getType();
+        BigDecimal amount = userTrade.getTradableAmount();
+        CurrencyPair currencyPair = userTrade.getCurrencyPair();
+        BigDecimal price = userTrade.getPrice();
 
+
+        BigDecimal feeAmount = userTrade.getFeeAmount();
+        String feeCurrency = userTrade.getFeeCurrency();
+        BigDecimal total = amount.multiply(price);
+        BigDecimal feesInCoins = price.multiply(feeAmount);
+        BigDecimal actualAmount = total.subtract(feesInCoins);
+
+        String message = actualAmount + " " + currencyPair.baseSymbol + " " + typeConvert(type) +
+                " for " + price + " " + currencyPair.counterSymbol +
+                " \n Fees Payed: " + feeAmount + " " + feeCurrency +
+                " \n Total Payed: " + total + " " + feeCurrency;
         return new SlackMessage(message, icon_emoji, username);
     }
+
+    private String typeConvert(OrderType type) {
+        String tradeType;
+        if (type == OrderType.BID) {
+            tradeType = purchased;
+        } else if (type == OrderType.ASK) {
+            tradeType = sold;
+        } else {
+            tradeType = "";
+        }
+        return tradeType;
+    }
+
+
 
 }
