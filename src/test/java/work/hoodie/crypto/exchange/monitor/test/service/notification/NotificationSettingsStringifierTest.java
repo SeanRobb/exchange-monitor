b28f27fb9f351d1293eb74fab318af6f2f48459a
SeanRobb <sean.robb@centurylink.com>
2016-03-22 19:08:06 -0500

    fun tests

--- src/test/java/work/hoodie/crypto/exchange/monitor/test/service/notification/NotificationSettingsStringifierTest.java
+++ src/test/java/work/hoodie/crypto/exchange/monitor/test/service/notification/NotificationSettingsStringifierTest.java
@@ -2,35 +2,68 @@ package work.hoodie.crypto.exchange.monitor.test.service.notification;
 
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.util.ReflectionTestUtils;
+import work.hoodie.crypto.exchange.monitor.domain.NotificationType;
 import work.hoodie.crypto.exchange.monitor.service.notification.NotificationSettingsStringifier;
+import work.hoodie.crypto.exchange.monitor.service.notification.NotificationTypeFinder;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
 
-/**
- * Created by sean on 3/22/16.
- */
+
 public class NotificationSettingsStringifierTest {
     @InjectMocks
     private NotificationSettingsStringifier classUnderTest;
 
-    private String emailAddress;
-    private String mailHost;
-    private int mailPort;
-    private String mailUsername;
-    private String mailPassword;
+    @Mock
+    private NotificationTypeFinder notificationTypeFinder;
+
+    private String emailAddress="EmailAddress@Email.com";
+    private String mailHost="Hostname";
+    private int mailPort=12;
+    private String mailUsername="UserNAme";
+    private String mailPassword="SecretPassword";
+    private String slackUrl="TopSecretSlackUrl";
 
     @Test
-    public void testGetSettings() {
+    public void testGetSettings_Email() {
         ReflectionTestUtils.setField(classUnderTest, "emailAddress", emailAddress);
         ReflectionTestUtils.setField(classUnderTest, "mailHost", mailHost);
         ReflectionTestUtils.setField(classUnderTest, "mailPort", mailPort);
         ReflectionTestUtils.setField(classUnderTest, "mailUsername", mailUsername);
         ReflectionTestUtils.setField(classUnderTest, "mailPassword", mailPassword);
 
-        String settings = classUnderTest.getSettings();
+        when(notificationTypeFinder.find()).thenReturn(NotificationType.EMAIL);
+
+        String actualMessage = classUnderTest.getSettings();
+
+        String expectedMessage = "Notification Type: " + NotificationType.EMAIL +
+                "Email Address: " + emailAddress +
+                "SMTP Mail Host: " + mailHost +
+                "SMTP Mail Port: " + mailPort +
+                "SMTP Mail Username: " + mailUsername +
+                "SMTP Mail Password: " + mailPassword;
+
+        assertNotNull(actualMessage);
+        assertEquals(expectedMessage, actualMessage);
+    }
 
 
-        assertTrue(true);
+    @Test
+    public void testGetSettings_Slack() {
+        ReflectionTestUtils.setField(classUnderTest, "slackUrl", slackUrl);
+
+        when(notificationTypeFinder.find()).thenReturn(NotificationType.SLACK);
+
+        String actualMessage = classUnderTest.getSettings();
+
+        String expectedMessage = "Notification Type: " + NotificationType.SLACK +
+                "Slack Url: " + slackUrl;
+
+        assertNotNull(actualMessage);
+        assertEquals(expectedMessage, actualMessage);
     }
 }
