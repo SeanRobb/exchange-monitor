779e2d04974c7024621e0dfdcc2db05a03c29fa5
SeanRobb <sean.robb@centurylink.com>
2016-11-16 21:47:29 -0600

    added a docker push script

--- scripts/dockerPush.sh
+++ scripts/dockerPush.sh
@@ -7,10 +7,10 @@ echo "Tag: ${TRAVIS_TAG}"
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     if [ -n ${TRAVIS_BRANCH} ]; then
-        mvn -f exchange-monitor-app/pom.xml docker:build -DpushImageTag -DdockerImageTag=${TRAVIS_BRANCH};
+        mvn -f exchange-monitor-app/pom.xml docker:build -DpushImageTag -DdockerImageTags=${TRAVIS_BRANCH};
     fi
     if [ -n ${TRAVIS_TAG} ]; then
-        mvn -f exchange-monitor-app/pom.xml docker:build -DpushImageTag -DdockerImageTag=${TRAVIS_TAG};
+        mvn -f exchange-monitor-app/pom.xml docker:build -DpushImageTag -DdockerImageTags=${TRAVIS_TAG};
     fi
     mvn -f exchange-monitor-app/pom.xml docker:build -DpushImageTag
 fi
