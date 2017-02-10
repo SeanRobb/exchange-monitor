a182ebdc22dbb2f18bff86835f8cdb316620207a
SeanRobb <sean.robb@centurylink.com>
2017-02-10 16:48:27 -0600

    implemented usertrade mongo dao

--- exchange-monitor-data/src/main/java/work/hoodie/exchange/monitor/data/dao/UserTradeDao.java
+++ exchange-monitor-data/src/main/java/work/hoodie/exchange/monitor/data/dao/UserTradeDao.java
@@ -1,13 +1,10 @@
 package work.hoodie.exchange.monitor.data.dao;
 
 
-import com.xeiam.xchange.dto.trade.UserTrade;
 import work.hoodie.exchange.monitor.common.RecentUserTrade;
 
-import java.util.List;
-
 public interface UserTradeDao {
-    RecentUserTrade save(List<UserTrade> userTradeList);
+    RecentUserTrade save(RecentUserTrade recentUserTrade);
 
     void delete(String id);
 
--- exchange-monitor-data/src/main/java/work/hoodie/exchange/monitor/data/dao/UserTradeMongoDao.java
+++ exchange-monitor-data/src/main/java/work/hoodie/exchange/monitor/data/dao/UserTradeMongoDao.java
@@ -1,25 +1,26 @@
-//package work.hoodie.exchange.monitor.data.dao;
-//
-//import work.hoodie.exchange.monitor.common.RecentUserTrade;
-//import work.hoodie.exchange.monitor.data.repository.UserTradeMongoRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-///**
-// * Created by ryantodd on 2/1/17.
-// */
-//@Component
-//public class UserTradeMongoDao implements UserTradeDao{
-//
-//    @Autowired
-//    private UserTradeMongoRepository userTradeMongoRepository;
-//
-//    public RecentUserTrade save(RecentUserTrade recentUserTrade) {
-//
-//        return userTradeMongoRepository.save(recentUserTrade);
-//    }
-//
-//    public void delete(String id) {
-//        userTradeMongoRepository.delete(id);
-//    }
-//
-//}
+package work.hoodie.exchange.monitor.data.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import work.hoodie.exchange.monitor.common.RecentUserTrade;
+import work.hoodie.exchange.monitor.data.repository.UserTradeMongoRepository;
+
+/**
+ * Created by ryantodd on 2/1/17.
+ */
+@Component
+public class UserTradeMongoDao implements UserTradeDao {
+
+    @Autowired
+    private UserTradeMongoRepository userTradeMongoRepository;
+
+    public RecentUserTrade save(RecentUserTrade recentUserTrade) {
+
+        return userTradeMongoRepository.save(recentUserTrade);
+    }
+
+    public void delete(String id) {
+        userTradeMongoRepository.delete(id);
+    }
+
+}
